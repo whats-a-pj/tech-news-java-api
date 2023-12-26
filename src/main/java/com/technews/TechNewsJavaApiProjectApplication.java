aa9998811270e8a03cafd1d89a88a861079c750b
Kaelyn P.J. Rasmussen <r4smussen.pj@gmail.com>
2023-12-25 22:30:34 -0700

    added java project

---
+++ src/main/java/com/technews/TechNewsJavaApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.technews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechNewsJavaApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechNewsJavaApiProjectApplication.class, args);
+	}
+
+}
